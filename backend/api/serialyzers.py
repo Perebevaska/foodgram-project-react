1b590a9f3bba9729648f0f0315747fa6cd916413
Dmitriy <dimkok27@gmail.com>
2023-04-12 00:14:51 +0500

    lazy fix

--- backend/api/serialyzers.py
+++ backend/api/serialyzers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 from ..users.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+from dj_rest_auth.registration.serializers import RegisterSerializer
 
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    def get_cleaned_data(self):
+        super().get_cleaned_data()
+        self.cleaned_data['first_name'] = self.validated_data.get('first_name', '')
+        self.cleaned_data['last_name'] = self.validated_data.get('last_name', '')
+        return self.cleaned_data
+
+
+
+#модифицировать с этим
     class Meta:
         model = User
         fields = '__all__'
+
+        class SignUpSerializer(serializers.Serializer):
+            email = serializers.EmailField(
+                required=True, max_length=settings.LEN_EMAIL, )
+            username = serializers.CharField(
+                max_length=settings.USER_LEN_NAME,
+                required=True,
+                validators=[UnicodeUsernameValidator(), validate_me_name],
+            )
