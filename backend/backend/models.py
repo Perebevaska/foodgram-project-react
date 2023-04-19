6c55db065fa1b112445e423c9b5a4539492d4a04
Dmitriy Kormin <dmitriykormin@yandex.ru>
2023-04-19 21:35:44 +0500

    models

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,46 +1,8 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from users.models import User
+from rest_framework.validators import ValidationError
 
-class Recipe(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='автор'
-    )
-    name = models.SlugField(
-        verbose_name='Название',
-        max_length=255,
-        unique=True,
-        blank=False
-    )
-    image = models.ImageField(
-        verbose_name='Картинка',
-        upload_to='backend/',
-        blank=False
-    )
-    description = models.TextField(
-        verbose_name='Текстовое описание',
-        blank=True,
-        null=True
-    )
-
-class Ingridient(models.Model):
-    name = models.SlugField(
-        verbose_name='Название',
-        max_length=255,
-        unique=True,
-        blank=False
-    )
-    quantity = models.IntegerField(
-        verbose_name='Количество',
-        max_length=4,
-        blank=False
-    )
-    measure_unit = models.SlugField(
-        verbose_name='Еденица измерения',
-        max_length=255,
-        blank=False
-    )
 
 class Tag(models.Model):
     name = models.CharField(
@@ -69,3 +31,63 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='автор'
+    )
+    name = models.SlugField(
+        verbose_name='Название',
+        max_length=255,
+        unique=True,
+        blank=False
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to='backend/',
+        blank=False
+    )
+    description = models.TextField(
+        verbose_name='Текстовое описание',
+        blank=True,
+        null=True
+    )
+    ingridients = models.ManyToManyField()
+    tags = models.ManyToManyField(Tag, related_name='recipes')
+    cooking_time = models.PositiveIntegerField(validators=[MinValueValidator(1, 'Время приготовления блюда не может быть меньше 1 минуты.')])
+
+class Ingredient(models.Model):
+    name = models.SlugField(
+        verbose_name='Название',
+        max_length=255,
+        unique=True,
+        blank=False
+    )
+    quantity = models.IntegerField(
+        verbose_name='Количество',
+        max_length=4,
+        blank=False
+    )
+    measure_unit = models.SlugField(
+        verbose_name='Еденица измерения',
+        max_length=255,
+        blank=False
+    )
+
+class IngredientAmount(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    amount = models.PositiveIntegerField(
+        validators=[MinValueValidator(1, 'Количество не может быть меньше 1')]
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='unique_ingredient_in_recipe'
+            )
+        ]
