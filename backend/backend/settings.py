7e00fce65204cbf0cf94a0ac74ca7c5021720f08
dimkok <dimkok27@gmail.com>
2023-05-08 14:22:48 +0500

    CSRF_TRUSTED_ORIGINS fix?

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['http://51.250.90.241']
 
 # Application definition
 
