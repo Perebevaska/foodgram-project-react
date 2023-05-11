37afaf7a6a53f1d6a2fb5cd7818d1d16cae71ea6
dimkok <dimkok27@gmail.com>
2023-05-11 22:08:01 +0500

    action fix

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
-                            ShoppingCart, Tag)
+                            CartList, Tag)
 from users.models import Subscription, User
 
 
