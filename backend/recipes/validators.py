e0e5ddab2a0e1e75a8f5ea0589af82708c2ca692
Dmitriy Kormin <dmitriykormin@yandex.ru>
2023-05-19 17:22:08 +0500

    models fix

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -1,6 +1,6 @@
 from rest_framework.validators import ValidationError as RFError
 
-from recipe.models import Ingredient, Tag
+from recipes.models import Ingredient, Tag
 
 
 def validate_ingredients(data):
