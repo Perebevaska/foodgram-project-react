f0b6285c37d7914ec8d0715d75cc5b247a139c7f
dimkok <dimkok27@gmail.com>
2023-05-20 00:41:47 +0500

    validators

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -2,46 +2,31 @@ from rest_framework.validators import ValidationError
 
 from recipes.models import Ingredient, Tag
 
-
-def validate_ingredients(data):
-    """Валидация ингредиентов и количества."""
+def validate_tags(data):
+    """Валидация тэгов."""
     if not data:
-        raise ValidationError({'ingredients': [
-            'Обязательное поле.']})
+        raise ValidationError({'tags': ['Обязательное поле.']})
     if len(data) < 1:
-        raise ValidationError({'ingredients': [
-            'Не переданы ингредиенты.']})
-    unique_ingredients = set()
-    for ingredient in data:
-        ingredient_id = ingredient.get('id')
-        if not ingredient_id:
-            raise ValidationError({'ingredients': [
-                'Отсутствует id ингредиента.']})
-        if not Ingredient.objects.filter(id=ingredient_id).exists():
-            raise ValidationError({'ingredients': [
-                'Ингредиента нет в БД.']})
-        if ingredient_id in unique_ingredients:
-            raise ValidationError({'ingredients': [
-                'Нельзя дублировать имена ингредиентов.']})
-        unique_ingredients.add(ingredient_id)
-        amount = int(ingredient.get('amount', 0))
-        if amount < 1:
-            raise ValidationError({'amount': [
-                'Количество не может быть менее 1.']})
+        raise ValidationError({'tags': ['Минимум 1 тэг.']})
+    for tag in data:
+        if not Tag.objects.filter(id=tag).exists():
+            raise ValidationError({'tags': ['Тэг отсутствует']})
     return data
 
-
-def validate_tags(data):
-    """Валидация тэгов: отсутствие
-    в request, отсутствие в БД."""
+def validate_ingredients(data):
+    """Валидация ингредиентов."""
     if not data:
-        raise ValidationError({'tags': [
-            'Обязательное поле.']})
+        raise ValidationError({'ingredients': ['Обязательное поле.']})
     if len(data) < 1:
-        raise ValidationError({'tags': [
-            'Хотя бы один тэг должен быть указан.']})
-    tag_ids = [tag.get('id') for tag in data]
-    if not Tag.objects.filter(id__in=tag_ids).count() == len(tag_ids):
-        raise ValidationError({'tags': [
-            'Один или несколько тэгов отсутствуют в БД.']})
+        raise ValidationError({'ingredients': ['Не переданы ингредиенты.']})
+    unique_ingredient = []
+    for ingredient in data:
+        id = ingredient.get('id')
+        if id in unique_ingredient:
+            raise ValidationError(
+                {'ingredients': ['Нельзя дублировать названия ингредиентов.']})
+        unique_ingredient.append(id)
+        amount = int(ingredient.get('amount'))
+        if amount < 1:
+            raise ValidationError({'amount': ['Количество не может быть меньше 1.']})
     return data
--- backend/scripts/management/commands/add_data.py
+++ backend/scripts/management/commands/add_data.py
@@ -1,32 +1,45 @@
 import os
-
+import csv
 import psycopg2
-from django.core.management.base import BaseCommand
-
-HOST = os.getenv('DB_HOST')
-NAME = os.getenv('DB_NAME')
-USER = os.getenv('POSTGRES_USER')
-PASSWORD = os.getenv('POSTGRES_PASSWORD')
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
     help = 'Добавление записей в БД из файлов .csv'
-    conn = psycopg2.connect(
-        f'host={HOST} dbname={NAME} user={USER} password={PASSWORD}'
-    )
-    cur = conn.cursor()
 
     def add_arguments(self, parser):
         """Аргументы для пути к файлу .csv и имени таблицы."""
         parser.add_argument('path', type=str, help='Путь к файлу .csv')
-        parser.add_argument(
-            'tab_name', type=str, help='Имя таблицы postgresql')
+        parser.add_argument('tab_name', type=str,
+                            help='Имя таблицы postgresql')
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
         table = kwargs['tab_name']
-        with open(path, 'r') as file:
-            self.cur.copy_from(
-                file, table, sep=',', columns=("name", "measurement_unit")
-            )
-        self.conn.commit()
+
+        # Валидация пути к файлу
+        if not os.path.isfile(path):
+            raise CommandError(f'Файл "{path}" не найден')
+
+        # Валидация имени таблицы
+        if not table.isidentifier():
+            raise CommandError(f'Невалидное имя таблицы "{table}"')
+
+        # Подключение к БД и курсор
+        conn = psycopg2.connect(
+            host=os.getenv('DB_HOST'),
+            dbname=os.getenv('DB_NAME'),
+            user=os.getenv('POSTGRES_USER'),
+            password=os.getenv('POSTGRES_PASSWORD'),
+        )
+        with conn, conn.cursor() as cur:
+            with open(path, 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Пропускаем заголовок
+                for row in reader:
+                    name, unit = row
+                    cur.execute(
+                        f'INSERT INTO {table} (name, measurement_unit) VALUES (%s, %s)',
+                        (name, unit)
+                    )
+            self.stdout.write(self.style.SUCCESS(f'Записи успешно добавлены в таблицу {table}'))
